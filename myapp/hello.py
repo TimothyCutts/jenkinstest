8542967cac63b7b9790dd2276dfa0c6726ddcbfb
Timothy.Cutts <timothy.cutts@rightware.com>
2023-07-07 03:46:02 -0700

    updated jenkinsfile

--- myapp/hello.py
+++ myapp/hello.py
@@ -3,12 +3,8 @@ import fire
 def hello(name="World"):
   return "Hello %s!" % name
 
-def add(num1, num2):
-  total = num1+num2
-  return total
 
 if __name__ == '__main__':
-  num1 = 1
-  num2 = 4
+
   fire.Fire(hello)
-  fire.Fire(add(num1, num2))
+  
